a3a8a70fe386f1bb6a7033f9609ae099099e6731
Laya Kishore <layakishore4947@gmail.com>
2025-03-08 13:23:22 +0530

    get & post api's

--- src/main/java/com/ecomproject/Ecom/Project/controller/ProductController.java
+++ src/main/java/com/ecomproject/Ecom/Project/controller/ProductController.java
@@ -1,14 +1,31 @@
 package com.ecomproject.Ecom.Project.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecomproject.Ecom.Project.entity.Product;
+import com.ecomproject.Ecom.Project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @RequestMapping("/home/{name}")
     public String greet(@PathVariable("name") String name) {
         return "Hello " + name;
+    }
+
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts()  {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
     }
 }
